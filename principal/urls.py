c57fecb6e47f9764998330b1b0871a1407ca3af7
TahianSylvain <ranjalahyandrytahianasylvain@gmail.com>
2023-07-12 16:03:59 +0300

    Implication du visiting

--- principal/urls.py
+++ principal/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path  # include
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import principal, delete, visiting,\
-    listing_tasks_by_time, search_results, generated
-from .api_views import AnnotationViewSet, TaskViewSet, \
-    _see_your_apis, _see_about_this_api, UserCreate, LoginView
+from .views import delete, search_results, generated, visiting  # listing_tasks_by_time, principal
+from .api_views import AnnotationViewSet, TaskViewSet
+# _see_your_apis, _see_about_this_api, UserCreate, LoginView
 
 
 router = routers.DefaultRouter()
@@ -15,10 +14,9 @@ router.register(r'annotation', AnnotationViewSet)
 
 app_name = 'main'
 urlpatterns = [
-    path('', generated, name='generat'),  # principal, name='entry'),
-    # path('generat/', generated, name='generat'),
+    path('', generated, name='entry'),
     path('search/', search_results, name='search'),
-
+    path('<int:annotation_id>/', generated, name='change'),
 
     # path('api/', include(router.urls)),
     # path('login/', LoginView.as_view(), name='login'),
@@ -28,7 +26,6 @@ urlpatterns = [
     # path('fullstack/<int:annotation_id>/', _see_about_this_api, name='about_steward'),
 
     # path('journey/on<str:annotate_deadline_date>/', listing_tasks_by_time, name='on_this_day'),
-    path('<int:annotation_id>/', principal, name='change'),
     path('foreign/<int:user_id>/', visiting, name='visiting'),
     path('del/<int:annotation_id>/', delete, name='delete'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -13,6 +13,8 @@ import openai
 from openai.error import APIConnectionError
 
 
+@sync_to_async()
+@login_required(login_url='account:login')
 def chatbot_response(user_input):
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -28,7 +30,10 @@ def chatbot_response(user_input):
 
 @sync_to_async()
 def visiting(request, user_id):
+    """ u = User.objects.all()"""
     utilisateur = get_object_or_404(User, pk=user_id)
+
+    # print(utilisateur.id)
     ses_annotations = get_list_or_404(Annotation, account=utilisateur)
     return render(
         request,
@@ -38,6 +43,8 @@ def visiting(request, user_id):
     )
 
 
+@sync_to_async()
+@login_required(login_url='account:login')
 def search_results(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest': # request.is_ajax():
         annotation = request.POST['annotation']
@@ -63,7 +70,6 @@ def search_results(request):
 
 @sync_to_async()
 def generated(request, annotation_id=0):
-
     if request.user.username:
         nature: str = "Add"
         advice: str = "Getting connection to openai..."
@@ -121,8 +127,8 @@ def generated(request, annotation_id=0):
                 note = Annotation.objects.get(id=annotation_id)
                 try:
                     advice = chatbot_response(  # string regular expression
-                        'I have a task named ' + note.name + r', that is described as this: ' + note.description +
-                        r'. Can You help me to make it well done as quick as possible?'
+                            'I have a task named ' + note.name + r', that is described as this: ' + note.description +
+                            r'. Can You help me to make it well done as quick as possible?'
                     )
                 except APIConnectionError:
                     response = 'Oh! It seems that connection has temporarily stopped!'
@@ -158,101 +164,8 @@ def generated(request, annotation_id=0):
         return render(request, template_name='./principal/generat.html', context={"dj": qs})
 
 
-@sync_to_async   # optimization performed
-def principal(request, annotation_id=0):
-    if request.user.username:
-        nature: str = "Add"
-        advice: str = "Getting connection to openai..."
-        length: float = 0
-        timing: str = ''
-        qs = Annotation.objects.filter(account=request.user).order_by("deadline")
-        for note in qs:
-            coming_deadline = note.deadline
-            delta = datetime.datetime(year=coming_deadline.year, month=coming_deadline.month,
-                                        day=coming_deadline.day, hour=coming_deadline.hour,
-                                        minute=coming_deadline.minute, second=coming_deadline.second, microsecond=0)
-            if (delta - datetime.datetime.now()).total_seconds() < 0 and note.over is False:
-                print(note)
-                note.over = True
-            elif (delta - datetime.datetime.now()).total_seconds() > 0 and note.over:
-                note.over = False
-            note.save()
-        if request.method == 'POST':
-            if annotation_id == 0:
-                desc = request.POST['description']
-                form = Annotation.objects.create(
-                    account=request.user,
-                    name=request.POST['name'],
-                    deadline=request.POST['deadline'],
-                    reminder=request.POST['reminder'], description=desc,
-                    over=False
-                )  # create
-                deadline = datetime.datetime(year=int(request.POST['deadline'][0:3]),
-                                            month=int(request.POST['deadline'][5:7]),
-                                            day=int(request.POST['deadline'][8:10]),
-                                            hour=int(request.POST['deadline'][11:13]),
-                                            minute=int(request.POST['deadline'][14:16]),
-                                            second=10, microsecond=10)
-                if (deadline - datetime.datetime.now()).total_seconds() > 0:
-                    over = False  # temps impartie finie
-                else:
-                    over = True
-
-                form.over = over
-                form.save()
-                return HttpResponseRedirect(reverse('main:entry'))
-            else:
-                nature = 'change'
-                note = Annotation.objects.get(id=annotation_id)
-                form = AnnotationForm(request.POST, instance=note)
-                if form.is_valid():
-                    form.save()
-                return HttpResponseRedirect(reverse('main:entry'))
-        elif request.method == 'GET':
-            if annotation_id == 0:
-                form = AnnotationForm()  # create
-            else:
-                nature = 'change'
-                note = Annotation.objects.get(id=annotation_id)
-                try:
-                    advice = chatbot_response(  # string regular expression
-                        'I have a task named ' + note.name + r', that is described as this: ' + note.description +
-                        r'. Can You help me to make it well done as quick as possible?'
-                    )
-                except APIConnectionError:
-                    response = 'Oh! It seems that connection has temporarily stopped!'
-                coming_deadline = note.deadline
-                delta = datetime.datetime(year=coming_deadline.year, month=coming_deadline.month,
-                                            day=coming_deadline.day, hour=coming_deadline.hour,
-                                            minute=coming_deadline.minute, second=coming_deadline.second, microsecond=0)
-                if delta > datetime.datetime.now():
-                    timing = 'blue'  # restful
-                    final = delta - datetime.datetime.now()
-                else:
-                    timing = 'red'  # late
-                    final = datetime.datetime.now() - delta
-                # 100% === 300px === 0s  // 0% === Opx === n_seconds  |
-                length = abs(round(100 * 3 / (1+(final.total_seconds()/1000))))
-                '''print('now it\'s', end=f'{datetime.datetime.now()}\n')'''
-                form = AnnotationForm(instance=note)
-            return render(request, template_name='principal/main.html', context={
-                        "dj": qs,
-                        "form": form,
-                        "type": nature,
-                        "advice": advice,
-                        "length": length,
-                        "timing": timing,
-                        }
-                    )
-    elif request.user:  # new_User_or_Anonymous
-        qs = User.objects.all()  # async filter(is_active=True, good_stat)
-
-        """ matplotlib(user, login) """
-
-        return render(request, template_name='./principal/main.html', context={"dj": qs})
-
-
 @sync_to_async
+@login_required(login_url='account:login')
 def listing_tasks_by_time(request, annotate_deadline_date):  # '%Y-%m-%d %H:%M:%S'
     """ http://localhost:3333/journey/on2023-05-19 18:32:24/ """
     annee = int(annotate_deadline_date[0:4])
@@ -272,7 +185,7 @@ def listing_tasks_by_time(request, annotate_deadline_date):  # '%Y-%m-%d %H:%M:%
 
 
 @sync_to_async()
-# @login_required(login_url="Larry's")
+@login_required(login_url="account:login")
 def delete(request, annotation_id):
     notif = Annotation.objects.get(id=annotation_id)
     if notif.account == request.user:  # only owner can delete his/her object
